5a7f52bd1c3f90f066f790d8aaea15d78038e1d9
Andrey Chuvashov <ch.andrey.2345@gmail.com>
2024-12-04 21:11:03 +0300

    feat: create sign-in page

--- AdminsProject/src/utils.py
+++ AdminsProject/src/utils.py
@@ -1,6 +1,6 @@
 SCREEN_SIZE = 1920, 1080
 
 
-def dp(value: int) -> int:
+def dp(value: int) -> float:
     """Функция для масштабирования элементов GUI."""
-    return value // 1.5
+    return value / 1.5
