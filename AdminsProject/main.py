9093144184afd21c4e4f9dfbde4c4b1982288784
Andrey Chuvashov <ch.andrey.2345@gmail.com>
2024-12-09 13:14:14 +0300

    feat: create password recovery page + do some refactoring + update routing

--- AdminsProject/main.py
+++ AdminsProject/main.py
@@ -3,11 +3,18 @@ from src import (
     WelcomePage,
     SignInPage,
     ProfilePage,
+    PasswordRecoveryPage,
 )
 
 
 async def main(page: ft.Page):
     """Главная функция запуска приложения."""
+    recovery_page = PasswordRecoveryPage(page=page)
+
+    async def on_switch_to_change_password(action) -> None:
+        """Переход на страницу восстановления пароля."""
+        await recovery_page.display(action=action)
+
     profile = ProfilePage(page=page)
 
     async def on_success(action) -> None:
@@ -17,6 +24,7 @@ async def main(page: ft.Page):
     sign_in = SignInPage(
         page=page,
         on_success=on_success,
+        to_recovery=on_switch_to_change_password,
     )
 
     async def on_switch_to_sign_in(action) -> None:
